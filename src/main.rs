3d5446df34b96e886b185288096c73bc625bbeb6
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-06 20:30:15 +0200

    new

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,42 @@
-const HEIGHT: usize = 10;
+use std::{thread, time};
+use rand::Rng;
+
+const HEIGHT: usize = 20;
 const WIDTH: usize = HEIGHT * 2;
 
 fn main() {
-    let alive: char = '@';
-    let  dead: char = '.';
+    let alive: char = 'o';
+    let  dead: char = ' ';
 
-    let mut states = [[false; WIDTH]; HEIGHT];
-    let mut next_states = states; 
+    let mut states = init_states();
+    let mut next_states = states;
 
-    states[4][4] = true;
-    states[4][5] = true;
-    states[4][6] = true;
-                
+    let wait_time = time::Duration::from_millis(500);
+
     loop {
+        thread::sleep(wait_time);
         for y in 0..HEIGHT {
             for x in 0..WIDTH {
                 let n = count_neighbours(x, y, &states);
                 compute_next(x, y, n, &states, &mut next_states);
-                draw(alive, dead, x, y, &states);
+                draw(alive, dead, x, y, states[y][x]);
             }
             println!("");
         }
         states = next_states;
+        print!("{}[2J", 27 as char); // clear terminal
     }
+}
+
+fn init_states() -> [[bool; WIDTH]; HEIGHT] {
+    let mut states = [[false; WIDTH]; HEIGHT];
+    for j in 0..HEIGHT {
+        for i in 0..WIDTH {
+            let val: bool = rand::thread_rng().gen_bool(0.5);
+            states[j][i] = val;
+        }
+    }
+    states
 }
 
 fn count_neighbours(x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) -> usize {
@@ -63,11 +77,19 @@ fn compute_next(x: usize, y: usize, n: usize,
     }
 }
 
-fn draw(alive: char, dead: char, x: usize, y: usize, states: &[[bool; WIDTH]; HEIGHT]) {
-    if states[y][x] {
+fn draw(alive: char, dead: char, x: usize, y: usize, state: bool) {
+    if state {
         print!("{}", alive);
     }
     else {
-        print!("{}", dead);
+        if y == 0 || y == HEIGHT - 1 {
+            print!("-");
+        }
+        else if x == 0 || x == WIDTH - 1 {
+            print!("|");
+        }
+        else {
+            print!("{}", dead);
+        }
     }
 }
